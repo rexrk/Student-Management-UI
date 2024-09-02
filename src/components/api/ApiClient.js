a92b5f16332ff943c9dffc54f2268700f3db47c0
Raman Kumar <kraman938@gmail.com>
2024-09-02 16:28:23 +0530

    fixed resource urls

--- src/components/api/ApiClient.js
+++ src/components/api/ApiClient.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.NODE_ENV === 'production' 
+  ? "http://student-springboot-app:8080"  // in kubernetes deployment
+  : "http://localhost:8080";
+
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL,
 });
